1b3d163d314d00fbaf9c66df98f669bdd4a50cc4
Administrator <Administrator@CLASS16>
2011-12-01 10:16:30 +0200

    hello world

---
+++ src/edu/course/hibernate/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.course.hibernate;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
